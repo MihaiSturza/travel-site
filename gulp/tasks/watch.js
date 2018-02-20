3386a805ca2fde7bcb7650a63097558edea3567b
Mihai <sturza.mimi@gmail.com>
2018-02-20 02:17:10 +0200

    CSS - packages for architecture and organization

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp'),
+    watch = require('gulp-watch');
+   /* browserSync = require('browser-sync').create();*/
+
+gulp.task('html', function() {
+	console.log('Amin');
+});
+   
+gulp.task('watch', function() {
+    /*browserSync.init({
+        notify: false,
+        server: {
+            baseDir: "app"
+        }
+    }); */
+    
+    watch('./app/index.html', function() {
+     gulp.start('html'); /*here, we would have instead browserSync.reload(); */
+    });
+    
+    watch('./app/assets/styles/**/*.css', function() {
+        gulp.start('styles'); /* here, if we want to use browserSync we'll place cssInject instead of styles, as in the cssInject task, it will only run after the styles would run */
+    });
+});
+
+
+/*
+gulp.task('cssInject', ['styles'], function() {
+    return gulp.scr('./app/temp/styles/styles.css')
+    .pipe(browserSync.stream());
+});*/
+
+//I chose not to use browserSync for for now us brackets has a browsersync included - realtime preview. Note that browserSync is a valuable tool also for cross browser compatibility testing, also while running it is bash we can take the adress and place it in a mobile device to view the webpage
---
+++ gulpfile.js
@@ -0,0 +1,2 @@
+require('./gulp/tasks/styles'); /*the require function will not require to put .js after styles */
+require('./gulp/tasks/watch');
